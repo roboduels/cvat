59fbed12fe6b6b8cba03b90c74b01d5f798ae9b4
sarikaya <sarikaya.nurefsan@gmail.com>
2022-05-23 15:21:15 +0300

    Implement grade-parameters from filename api

--- cvat/apps/engine/urls.py
+++ cvat/apps/engine/urls.py
@@ -76,5 +76,5 @@ urlpatterns = [
     path('api/v1/', include((router.urls, 'cvat'), namespace='v1')),
     path('api/v1/cvat-grades', views.post_grades),
     path('api/v1/grade-parameters', views.GradeParametersFromCertificateView.as_view(), name="cron-helper"),
-    path('api/v1/grade-parameters-filename', views.GradeParametersFromFileNameView.as_view(), name="cron-helper-filename")
+    path('api/v1/grade-parameters-filename', views.GradeParametersFromFileNameView.as_view(), name="grade-parameters-filename")
 ]
