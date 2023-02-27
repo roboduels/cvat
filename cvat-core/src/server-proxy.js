a1daebd1df0f96f1aa2ca4305a9eb39585be7068
sarikaya <sarikaya.nurefsan@gmail.com>
2023-02-28 00:59:27 +0300

    Revert Log onDownloadProgress event from axios request

--- cvat-core/src/server-proxy.js
+++ cvat-core/src/server-proxy.js
@@ -881,13 +881,6 @@
                         {
                             proxy: config.proxy,
                             responseType: 'arraybuffer',
-                            onDownloadProgress: progressEvent => {
-                              console.log('loaded', progressEvent.loaded);
-                              console.log('total', progressEvent.total);
-                              console.log('responseHeaders', progressEvent.currentTarget.responseHeaders);
-                              let percentCompleted = Math.floor(progressEvent.loaded / progressEvent.total * 100)
-                              console.log('completed: ', percentCompleted)
-                            }
                         },
                     );
                 } catch (errorData) {
